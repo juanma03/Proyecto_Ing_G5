8c1a988e9cab329adf252e77ef8642d794718988
juanma <juanma.m21@hotmail.com>
2014-12-12 14:16:35 -0300

    Version 3.0

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
- devise_for :users
+ devise_for :users, :controllers => { :registrations => 'registrations' }
+ 
  get '/mis_publicacions' => 'publications#mis_publicacions'
  get '/statistics/por_post'
  post '/statistics/por_post'
