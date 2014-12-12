443d81d8e1b7bbb4d1f7e89a6730c838f7accba2
juanma <juanma.m21@hotmail.com>
2014-12-12 01:07:20 -0300

    Verison 3.0
    application rb
    config.assets.precompile += %w( application.js )

--- BestnidWeb/config/application.rb
+++ BestnidWeb/config/application.rb
@@ -5,6 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+config.assets.precompile += %w( application.js )
 
 module SimpleFormBootstrap
   class Application < Rails::Application
