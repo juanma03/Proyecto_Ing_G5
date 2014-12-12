4d79a68d8e7984d1f67a27e07fbb85f9e8799b5f
juanma <juanma.m21@hotmail.com>
2014-12-11 21:38:05 -0300

    Version 3.0

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -4,6 +4,6 @@ class Offer < ActiveRecord::Base
 
 	validates_format_of :amount, :with => /\A[0-9.]{1,10}\Z/
 	validates_presence_of :necessity
-  validates_format_of :security_code, :with => /\A[0-9.]{4}\Z/
+  validates_format_of :security_code, :with => /\A[0-9.]{3}\Z/
 
 end
